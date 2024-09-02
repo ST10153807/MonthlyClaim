0adf469ef040ef4abbf5327610aed70c8fb95a02
ST10153807 <ST10153807@rcconnect.edu.za>
2024-09-02 20:10:21 +0200

    Faculty Model

---
+++ Models/Faculty.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MonthlyClaim.Models
+{
+    public class Faculty
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String Logo { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+    }
+}
